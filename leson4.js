7e436652afab821c2daca9d980b88eacf68746ce
ERIKbmw <67366683+ERIKbmw@users.noreply.github.com>
2020-10-25 14:40:41 +0400

    Add files via upload

---
+++ leson4.js
@@ -0,0 +1,19 @@
+var a =prompt("Մուտքագրեք բառը:")
+if(a == "red"){
+    alert('Կարմիր')}
+    
+else if(a == "car"){
+    alert('Ավտոմեքենա')
+}
+else if(a == "house"){
+    alert('Տուն')
+}
+else if(a == "computer"){
+    alert('համակարգիչ')
+}
+else if(a == "phone"){
+    alert('Հեռախոս')
+}else{
+    alert('Այս բառը բառարանում չի գտնվել: Հետագայում կավելացնենք:')
+}
+
